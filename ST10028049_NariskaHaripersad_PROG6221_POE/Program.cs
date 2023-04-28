07d0a926ab06b4dd8a48ae3c08839f0efda3bc6e
Naris <Naris@DESKTOP-JP19TVO>
2023-04-28 13:49:45 +0200

    The methods that will perform functions on the recipe ingredients are created in the Ingredients class. Comments are added explaining the variable declarations in the class and what each method in the class does.

--- ST10028049_NariskaHaripersad_PROG6221_POE/Program.cs
+++ ST10028049_NariskaHaripersad_PROG6221_POE/Program.cs
@@ -6,17 +6,40 @@ using System.Threading.Tasks;
 
 namespace ST10028049_NariskaHaripersad_PROG6221_POE
 {
-    class Ingredient
+    class Ingredient // contains the methods that will perform functions on the recipe ingredients 
     {
-        private string[] ingredientNames;
-        private double[] ingredientQuantities;
-        private string[] ingredientUnits;
-        private int numOfIngredients;
-        private string name;
-        private double quantity;
-        private string unit;
-        private bool isInputValid;
+        // variable declarations 
+        private string[] ingredientNames; // an array that stores the ingredient names 
+        private double[] ingredientQuantities; //an array that stores the ingredient quantities 
+        private string[] ingredientUnits; //an array that stores the unit of measurements
+        private int numOfIngredients; // stores the number of ingredients
+        private string name; // name of the ingredient 
+        private double quantity; // the quantity of the ingredient 
+        private string unit; // the unit of measurement 
+        private bool isInputValid; // used to determine whether an inputted value is valid or not
+
+        public void getIngredients() // a method that allows the user to input details about the recipe ingredients and stores it in memory
+        {
+            
+        }
+
+        public void printIngredients() // a method that displays the ingredient details 
+        {
+            
+        }
+
+        public void scaleIngredients(int scale) // a method that scales the ingredients 
+        {
+
+        }
+
+        public void resetIngredients(int scale) // a method that resets the recipe back to its original
+        {
+            
+        }
+
     }
+     
         internal class Program
     {
         static void Main(string[] args)
